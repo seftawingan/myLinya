c823c425f1db51b4c51972bcc49b47abb704ff19
seftawingan <janjosefliza.tawingan25@bicol-u.edu.ph>
2026-01-29 00:08:25 +0800

    paks ok na ba yan

--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-import { Link } from "./components";
-import MainPage from "./components/mainPage";
+import { Link } from "./src/app/frontend/components";  
+import MainPage from "./src/app/frontend/components/mainPage";
 
 export default function Page() {
   return (
